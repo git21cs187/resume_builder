e3b8a038e725287248e2d1ba46844bd12fc0cb50
Sneha <124282444+git21cs187@users.noreply.github.com>
2023-12-26 17:21:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+//alert("loading");
+function addNewWEField(){
+    //console.log("Adding new field");
+
+let newNode=document.createElement("textarea");
+newNode.classList.add("form-control");
+newNode.classList.add("weField");
+newNode.classList.add("mt-2");
+newNode.setAttribute("rows", 3);
+newNode.setAttribute("placeholder","Enter here")
+
+let weOb=document.getElementById("we");
+let weAddButtonOb=document.getElementById("weAddButton");
+
+weOb.insertBefore(newNode,weAddButtonOb);
+}
+
+function addNewAQField(){
+    
+let newNode=document.createElement("textarea");
+newNode.classList.add("form-control");
+newNode.classList.add("eqField");
+newNode.classList.add("mt-2");
+newNode.setAttribute("rows", 3);
+newNode.setAttribute("placeholder","Enter here")
+
+let aqOb=document.getElementById("aq");
+let aqAddButtonOb=document.getElementById("aqAddButton");
+
+aqOb.insertBefore(newNode,aqAddButtonOb);
+
+}
+
+function addNewIPField(){
+    
+    let newNode=document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("ipField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder","Enter here")
+    
+    let ipOb=document.getElementById("intsprojs");
+    let ipAddButtonOb=document.getElementById("ipAddButton");
+    
+    ipOb.insertBefore(newNode,ipAddButtonOb);
+    
+    }
+
+//generating CV
+function generateCV(){
+    //console.log("Generating CV");
+
+    let nameField=document.getElementById('nameField').value;
+
+    let nameT1=document.getElementById('nameT1')
+
+    nameT1.innerHTML=nameField;
+
+    //direct
+    document.getElementById('nameT2').innerHTML=nameField;
+
+
+    //contacts
+    document.getElementById('contactT').innerHTML=document.getElementById('contactField').value;
+
+    //address
+
+    document.getElementById('addressT').innerHTML=document.getElementById('addressField').value;
+
+    document.getElementById('fbT').innerHTML=document.getElementById('fbField').value;
+
+    document.getElementById('instaT').innerHTML=document.getElementById('instaField').value;
+
+    document.getElementById('linkedInT').innerHTML=document.getElementById('LinkedInField').value;
+
+    document.getElementById('hob1T').innerHTML=document.getElementById('hobbiesField').value;
+    
+
+    //objective
+
+    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
+
+    //we
+
+    let wes=document.getElementsByClassName("weField");
+
+    let str =" ";
+
+    for (let e of wes) {
+        str= str+`<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("weT").innerHTML= str;
+
+    //aq
+
+    let aqs=document.getElementsByClassName("eqField");
+
+    let str1="";
+
+    for(let e of aqs){
+        str1+=`<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("aqT").innerHTML=str1;
+
+    //ints and projects
+
+    let ints=document.getElementsByClassName("ipField");
+
+    let str2="";
+
+    for(let e of ints){
+        str2+=`<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("intT").innerHTML=str2;
+
+    //code for setting image
+
+    let file=document.getElementById("imgField").files[0];
+    console.log(file);
+    let reader=new FileReader();
+    reader.readAsDataURL(file);
+    console.log(reader.result);
+    //set image to template
+
+    reader.onloadend=function(){
+        document.getElementById("imgTemplate").src=reader.result;
+    };
+
+    document.getElementById("cv-form").style.display="none";
+    document.getElementById("cv-template").style.display="block";
+
+
+
+}
+
+//printing cv
+function printCV(){
+    window.print();
+}
